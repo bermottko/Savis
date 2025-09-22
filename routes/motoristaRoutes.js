ecc10eb91141e20e0c58576ab2b6c3853a358a72
luisafrancca <luisafrancca@gmail.com>
2025-09-21 23:18:49 -0300

    Relatório do motorista

--- routes/motoristaRoutes.js
+++ routes/motoristaRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const motoristaController = require('../controllers/motoristaController');
 
 router.get('/usuarios/index', motoristaController.renderUsuarios);
+
 router.get('/viagens/index', motoristaController.renderViagens);
+router.get('/viagens/lista', motoristaController.renderViagensLista);
 router.get('/viagens/buscar-eventos', motoristaController.renderBuscarEventos);
 router.get('/viagens/ver-viagem/:cod', motoristaController.renderVerViagem);
+router.get('/viagens/participantes/:cod', motoristaController.verParticipantes);
+
+router.get('/viagens/relatorio/:cod', motoristaController.renderRelatorio);
+router.put('/viagens/relatorio/:cod', motoristaController.salvarRelatorio);
 
 module.exports = router;
